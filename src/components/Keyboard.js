0f920d9791a57dcfe7df5bc34a05c4e89c982246
Simran Kaur <simran00kk@gmail.com>
2024-03-05 12:05:45 -0500

    Text now showing for enter and delete buttons. Enter and Delete functioning as expected. User is no longer limited to the first line.

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,13 +1,52 @@
-import React, {useEffect} from 'react'
+import React, {useCallback, useEffect, useContext} from 'react'
 import Key from './Key'
+import {AppContext} from '../App'
 
 function Keyboard() {
+  const { onDelete,onEnter,onSelectLetter } = useContext(AppContext)
+
   const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
 
+  const handleKeyboard = useCallback((event)=>{
+    if (event.key === "Enter"){
+      onEnter()
+
+    } else if (event.key ==="Backspace"){
+      onDelete()
+    } else {
+      // need to use forEach to determine 
+      // can we just do onSelectLetter passing in event.key?
+      keys1.forEach(key => {
+        if (event.key === key){
+          onSelectLetter(key)
+        }
+      })
+      keys2.forEach(key => {
+        if (event.key === key){
+          onSelectLetter(key)
+        }
+      })
+      keys3.forEach(key => {
+        if (event.key === key){
+          onSelectLetter(key)
+        }
+      })
+
+    }
+
+  })
+
+  useEffect(()=>{
+    document.addEventListener("keydown",handleKeyboard)
+    return () =>{
+      document.removeEventListener("keydown",handleKeyboard)
+    }
+  },[handleKeyboard])
+
   return (
-    <div className='keyboard'>
+    <div className='keyboard' onKeyDown={handleKeyboard}>
 
       <div className='line1'>
         {keys1.map((key)=>{
@@ -20,10 +59,10 @@ function Keyboard() {
       </div>
 
       <div className='line3'>
-        <Key keyval={"ENTER"} bigKey/>
+        <Key keyVal={"ENTER"} bigKey/>
         {keys3.map((key)=>{
           return <Key keyVal = {key}/>})}
-        <Key keyval={"DELETE"} bigKey/>
+        <Key keyVal={"DELETE"} bigKey/>
       </div>
 
     </div>
